2e909163a5d50a0d151c9856e4bf9624131e9e3a
Zhansaya Ayazbayeva <zh.ayazbayeva@gmail.com>
2021-01-27 19:54:29 -0800

    Initial Commit

---
+++ SetGame/SetGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  SetGameApp.swift
+//  SetGame
+//
+//  Created by Zhansaya Ayazbayeva on 2021-01-27.
+//
+
+import SwiftUI
+
+@main
+struct SetGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
